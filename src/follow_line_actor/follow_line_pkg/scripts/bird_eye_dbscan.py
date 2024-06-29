4df22797b78db0f49a0ea38fc6e0f3da4851be27
Beñat Froemming <117485318+benatfroemming@users.noreply.github.com>
2024-06-29 17:32:05 -0400

    birds eye view + dbscan + dbw

--- src/follow_line_actor/follow_line_pkg/scripts/bird_eye_dbscan.py
+++ src/follow_line_actor/follow_line_pkg/scripts/bird_eye_dbscan.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import Empty
 from cv_bridge import CvBridge, CvBridgeError
 from dynamic_reconfigure.server import Server
-from follow_lane_pkg.cfg import FollowLaneConfig  # packageName.cfg
+from follow_line_pkg.cfg import FollowLineConfig  # packageName.cfg
 from geometry_msgs.msg import Twist
 import cv2 as cv
 import numpy as np
 from sklearn.cluster import DBSCAN
+import time
+
+# dbw commands
 import math
+from dataspeed_ulc_msgs.msg import UlcCmd # Drive by wire UL 
+from dbw_polaris_msgs.msg import SteeringCmd # drive by wire native messages
 
 # global variables
 vel_msg = Twist()
 bridge = CvBridge()
 speed = 0
 drive = False
-velocity_pub = None
+velocity_pub = None 
+start_time = None
+start_time2 = None
 
 # dynamic reconfigure
 def dyn_rcfg_cb(config, level):
-    global speed, drive
+    global speed, drive, ly, uy, ls, lv, us, uv
     speed = config.speed
     drive = config.enable_drive
+    ly = config.lower_yellow
+    uy = config.upper_yellow
+    ls = config.ls
+    lv = config.lv
+    us = config.us
+    uv = config.uv
     return config
 
 # compute lines and follow lane
@@ -122,6 +136,7 @@ def compute_lines(rows, image):
     if len(largest_labels) == 0:
         return None
     
+    # if only one cluster
     elif len(largest_labels) == 1:
         lane_points = points[clusters == largest_labels[0]]
         centroid_x = lane_points[np.argmax(lane_points[:, 0])][1]
@@ -176,18 +191,9 @@ def compute_lines(rows, image):
     cv.waitKey(3) 
     return cx, cy, cols, rows
 
-# convert angle to yaw rate
-def angle_to_yaw(angle_degrees, max_steer_angle=11.25, max_yaw_rate=1.0):
-
-    angle_degrees = min(max_steer_angle, angle_degrees)
-    angle_degrees = (angle_degrees + 180) % 360 - 180  # normalize the angle to the range [-180, 180]
-    angle_radians = math.radians(angle_degrees)
-    yaw_rate = max_yaw_rate * angle_radians / math.radians(max_steer_angle) # scale the angle to the maximum yaw rate
-    return yaw_rate
-
 # image callback
 def image_callback(ros_image):
-    global bridge, vel_msg, speed, drive, velocity_pub
+    global bridge, vel_msg, speed, drive, velocity_pub, start_time, start_time2
 
     try:
         cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
@@ -201,36 +207,132 @@ def image_callback(ros_image):
     # compute lines and obtain ideal image center
     cx, cy, cols, rows = compute_lines(rows, image)
 
+    image2 = cv_image.copy()
+    image2 = image2[int(rows*3/5):int(rows*4/5), int(cols * 1 / 3): int(cols * 2/3)]
+    rows2, cols2, _ = image2.shape
+    # yellow mask
+    hsv = cv.cvtColor(image2,cv.COLOR_BGR2HSV)
+    lower_yellow = np.array([ly, ls, lv]) 
+    upper_yellow = np.array([uy, us, uv])
+    yellow_mask = cv.inRange(hsv, lower_yellow, upper_yellow)
+
+    # check for intersection
+    num_yellow_pix = cv.countNonZero(yellow_mask)
+    yellow_pct = (100 * num_yellow_pix) / (rows2 * cols2)
+    print(yellow_pct)
+
+    # show image
+    cv.imshow("yellow", yellow_mask)
+    cv.waitKey(3)    
+
     # get turn angle
     mid_x = cols // 2
-    angle = float(math.degrees(math.atan2(abs(mid_x - cx), abs(rows - cy)))) * 0.75
+    angle = float(math.degrees(math.atan2(abs(mid_x - cx), abs(rows - cy)))) 
 
     # compute linear and angular speed
+    target_speed = speed #mph
     if cx:
+        mid = cols / 2
         if drive:
-            vel_msg.linear.x = speed
+            if start_time2 is not None:
+                current_time = time.time()
+                if (current_time - start_time2) < 20:
+                    publish_ulc_speed(speed)
+                else:
+                    start_time2 = None
+            elif yellow_pct > 5 and start_time == None:
+                start_time = time.time()
+            elif start_time is not None:
+                current_time = time.time()
+                if (current_time - start_time) < 5:
+                    publish_ulc_speed(0)
+                else:
+                    start_time = None
+                    start_time2 = time.time()
+            else:
+                publish_ulc_speed(target_speed)
         else:
-            vel_msg.linear.x = 0
+            publish_ulc_speed(0)
 
         angular_threshold = 20
-        if mid_x < cx - angular_threshold:
-            vel_msg.angular.z = -angle_to_yaw(angle)
-        elif mid_x > cx + angular_threshold:
-            vel_msg.angular.z = angle_to_yaw(angle)
+        if mid < cx - angular_threshold:
+            publish_steering(-angle)
+        elif mid > cx + angular_threshold:
+            publish_steering(angle)
         else:
-            vel_msg.angular.z = 0
-        velocity_pub.publish(vel_msg)
-    else:
-        vel_msg.angular.z = 0
-        velocity_pub.publish(vel_msg)
+            publish_steering(0)
+    enable_dbw()
 
+# publish linear speed
+def publish_ulc_speed(speed: float) -> None:
+    """Publish requested speed to the vehicle using ULC message."""
+
+    ulc_cmd = UlcCmd()
+    ulc_cmd.enable_pedals = True
+    ulc_cmd.enable_steering = False  # NOTE: Steering control via ULC is not used here
+    ulc_cmd.enable_shifting = True
+    ulc_cmd.shift_from_park = True
+
+    ulc_cmd.linear_velocity = round((speed / 2.237), 3)  # Convert mph to m/s
+    ulc_cmd.linear_accel = 0.0
+    ulc_cmd.linear_decel = 0.0
+    ulc_cmd.jerk_limit_throttle = 0.0
+    ulc_cmd.jerk_limit_brake = 0.0
+
+    ulc_cmd.pedals_mode = 0  # Speed mode
+    # ---------------------------------------------------------------------
+    pub_ulc.publish(ulc_cmd)
+
+# publish steering
+def publish_steering(requested_road_angle: float = None) -> None:
+    """Publish requested steering to the vehicle.
+    Input can be desired degree road angle (-37 to 37) or steering angle (-600 to 600)"""
+
+    if requested_road_angle is None:
+        rospy.logerr("publish_steering called with no steering angle provided")
+        requested_road_angle = 0
+
+    if requested_road_angle is not None:
+        if requested_road_angle < 0:
+            requested_road_angle = max(requested_road_angle, -37) * 16.2
+        else:
+            requested_road_angle = min(requested_road_angle, 37) * 16.2
+
+    # Make steering message -----------------------------------------------
+    msg_steering = SteeringCmd()
+    msg_steering.steering_wheel_angle_cmd = math.radians(requested_road_angle)
+    # NOTE: (-600deg to 600deg converted to radians)
+    msg_steering.enable = True  # Enable Steering, required 'True' for control via ROS
+
+    # Do NOT use these without completely understanding how they work on the hardware level:
+    msg_steering.cmd_type = SteeringCmd.CMD_ANGLE  # CAUTION: Torque mode disables lateral acceleration limits
+    # Use angle velocity to control rate. Lock to lock = 1200deg i.e. 300deg/s will be 4secs lock to lock
+    msg_steering.steering_wheel_angle_velocity = math.radians(300)  # deg/s -> rad/s
+    msg_steering.steering_wheel_torque_cmd = 0.0  # Nm
+    msg_steering.clear = False
+    msg_steering.ignore = False
+    msg_steering.calibrate = False
+    msg_steering.quiet = False
+    msg_steering.count = 0
+    # ---------------------------------------------------------------------
+    pub_steering.publish(msg_steering)
+
+# enable vehicle
+def enable_dbw() -> None:
+    """Enable vehicle control using ROS messages"""
+    msg = Empty()
+    pub_enable_cmd.publish(msg)
+
+# main method
 if __name__ == '__main__':
-    rospy.init_node('bird_eye_dbscan', anonymous=True)
-    imgtopic = rospy.get_param("~imgtopic_name")
-    rospy.Subscriber(imgtopic, Image, image_callback)
-    velocity_pub = rospy.Publisher('/robot1/cmd_vel', Twist, queue_size=1)
-    srv = Server(FollowLaneConfig, dyn_rcfg_cb)
-    try:
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+  rospy.init_node('birds_eye_dbscan', anonymous=True) # initialize node
+  imgtopic = rospy.get_param("~imgtopic_name") # private name
+  rospy.Subscriber(imgtopic, Image, image_callback) # subscribe to image (so that image_callback can be called every time an image is published)
+  pub_ulc = rospy.Publisher("/vehicle/ulc_cmd", UlcCmd, queue_size=1)
+  pub_steering = rospy.Publisher("/vehicle/steering_cmd", SteeringCmd, queue_size=1)
+  pub_enable_cmd = rospy.Publisher("/vehicle/enable", Empty, queue_size=1)
+  srv = Server(FollowLineConfig, dyn_rcfg_cb) # create dynamic reconfigure server that calls dyn_rcfg_cb function every time a parameter is changed
+  try:
+    rospy.spin()
+  except rospy.ROSInterruptException:
+    pass
