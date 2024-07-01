12647c1b50ef05c3f3f2754a985f577298a502b3
rickey johnson <rdjwinter2004@gmail.com>
2024-07-01 14:47:35 -0400

    who cares, whatever

--- src/actor_demo/follow_line_pkg/scripts/advanced_dbw_follow_lane.py
+++ src/actor_demo/follow_line_pkg/scripts/advanced_dbw_follow_lane.py
@@ -292,64 +292,64 @@ def image_callback(ros_image):
             publish_steering(0)
     enable_dbw()
     
-def publish_ulc_speed(speed: float) -> None:
-    """Publish requested speed to the vehicle using ULC message."""
+# def publish_ulc_speed(speed: float) -> None:
+#     """Publish requested speed to the vehicle using ULC message."""
 
-    ulc_cmd = UlcCmd()
-    ulc_cmd.enable_pedals = True
-    ulc_cmd.enable_steering = False  # NOTE: Steering control via ULC is not used here
-    ulc_cmd.enable_shifting = True
-    ulc_cmd.shift_from_park = True
+#     ulc_cmd = UlcCmd()
+#     ulc_cmd.enable_pedals = True
+#     ulc_cmd.enable_steering = False  # NOTE: Steering control via ULC is not used here
+#     ulc_cmd.enable_shifting = True
+#     ulc_cmd.shift_from_park = True
 
-    ulc_cmd.linear_velocity = round((speed / 2.237), 3)  # Convert mph to m/s
-    ulc_cmd.linear_accel = 0.0
-    ulc_cmd.linear_decel = 0.0
-    ulc_cmd.jerk_limit_throttle = 0.0
-    ulc_cmd.jerk_limit_brake = 0.0
+#     ulc_cmd.linear_velocity = round((speed / 2.237), 3)  # Convert mph to m/s
+#     ulc_cmd.linear_accel = 0.0
+#     ulc_cmd.linear_decel = 0.0
+#     ulc_cmd.jerk_limit_throttle = 0.0
+#     ulc_cmd.jerk_limit_brake = 0.0
 
-    ulc_cmd.pedals_mode = 0  # Speed mode
-    # ---------------------------------------------------------------------
-    pub_ulc.publish(ulc_cmd)
-
-
-def publish_steering(requested_road_angle: float = None) -> None:
-    """Publish requested steering to the vehicle.
-    Input can be desired degree road angle (-37 to 37) or steering angle (-600 to 600)"""
-
-    if requested_road_angle is None:
-        rospy.logerr("publish_steering called with no steering angle provided")
-        requested_road_angle = 0
-
-    if requested_road_angle is not None:
-        if requested_road_angle < 0:
-            requested_road_angle = max(requested_road_angle, -37) * 16.2
-        else:
-            requested_road_angle = min(requested_road_angle, 37) * 16.2
-
-    # Make steering message -----------------------------------------------
-    msg_steering = SteeringCmd()
-    msg_steering.steering_wheel_angle_cmd = math.radians(requested_road_angle)
-    # NOTE: (-600deg to 600deg converted to radians)
-    msg_steering.enable = True  # Enable Steering, required 'True' for control via ROS
-
-    # Do NOT use these without completely understanding how they work on the hardware level:
-    msg_steering.cmd_type = SteeringCmd.CMD_ANGLE  # CAUTION: Torque mode disables lateral acceleration limits
-    # Use angle velocity to control rate. Lock to lock = 1200deg i.e. 300deg/s will be 4secs lock to lock
-    msg_steering.steering_wheel_angle_velocity = math.radians(300)  # deg/s -> rad/s
-    msg_steering.steering_wheel_torque_cmd = 0.0  # Nm
-    msg_steering.clear = False
-    msg_steering.ignore = False
-    msg_steering.calibrate = False
-    msg_steering.quiet = False
-    msg_steering.count = 0
-    # ---------------------------------------------------------------------
-    pub_steering.publish(msg_steering)
+#     ulc_cmd.pedals_mode = 0  # Speed mode
+#     # ---------------------------------------------------------------------
+#     pub_ulc.publish(ulc_cmd)
 
 
-def enable_dbw() -> None:
-    """Enable vehicle control using ROS messages"""
-    msg = Empty()
-    pub_enable_cmd.publish(msg)
+# def publish_steering(requested_road_angle: float = None) -> None:
+#     """Publish requested steering to the vehicle.
+#     Input can be desired degree road angle (-37 to 37) or steering angle (-600 to 600)"""
+
+#     if requested_road_angle is None:
+#         rospy.logerr("publish_steering called with no steering angle provided")
+#         requested_road_angle = 0
+
+#     if requested_road_angle is not None:
+#         if requested_road_angle < 0:
+#             requested_road_angle = max(requested_road_angle, -37) * 16.2
+#         else:
+#             requested_road_angle = min(requested_road_angle, 37) * 16.2
+
+#     # Make steering message -----------------------------------------------
+#     msg_steering = SteeringCmd()
+#     msg_steering.steering_wheel_angle_cmd = math.radians(requested_road_angle)
+#     # NOTE: (-600deg to 600deg converted to radians)
+#     msg_steering.enable = True  # Enable Steering, required 'True' for control via ROS
+
+#     # Do NOT use these without completely understanding how they work on the hardware level:
+#     msg_steering.cmd_type = SteeringCmd.CMD_ANGLE  # CAUTION: Torque mode disables lateral acceleration limits
+#     # Use angle velocity to control rate. Lock to lock = 1200deg i.e. 300deg/s will be 4secs lock to lock
+#     msg_steering.steering_wheel_angle_velocity = math.radians(300)  # deg/s -> rad/s
+#     msg_steering.steering_wheel_torque_cmd = 0.0  # Nm
+#     msg_steering.clear = False
+#     msg_steering.ignore = False
+#     msg_steering.calibrate = False
+#     msg_steering.quiet = False
+#     msg_steering.count = 0
+#     # ---------------------------------------------------------------------
+#     pub_steering.publish(msg_steering)
+
+
+# def enable_dbw() -> None:
+#     """Enable vehicle control using ROS messages"""
+#     msg = Empty()
+#     pub_enable_cmd.publish(msg)
 
 
 
