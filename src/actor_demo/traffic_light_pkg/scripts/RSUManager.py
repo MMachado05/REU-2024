59e882e77ff95bfba8ff814b88024eb879e82694
TatianaRas <tanya.rastoskueva@gmail.com>
2024-07-01 15:18:06 -0400

    Add image publish in RSU

--- src/actor_demo/traffic_light_pkg/scripts/RSUManager.py
+++ src/actor_demo/traffic_light_pkg/scripts/RSUManager.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import rospy
+import rospkg
+import cv2
+import numpy
+from sensor_msgs.msg import Image
 from dynamic_reconfigure.server import Server
 # from ....cfg import Config
 from std_msgs.msg import Bool
 from std_msgs.msg import Time
 import threading
+from cv_bridge import CvBridge, CvBridgeError
 
 class RSUManager:
   
@@ -19,15 +24,27 @@ class RSUManager:
         self.north_pub = rospy.Publisher("/north/state", Bool, queue_size=10)
         self.south_pub = rospy.Publisher("/south/state", Bool, queue_size=10)
         
+        self.north_image = rospy.Publisher("/north/image", Image, queue_size=10)
+        self.south_image = rospy.Publisher("/south/image", Image, queue_size=10)
+        
         self.timeN_pub = rospy.Publisher("/north/time_to_state", Time, queue_size=1)
         self.timeS_pub = rospy.Publisher("/south/time_to_state", Time, queue_size=1)
-        # self.srv = Server(_Config, self.dyn_rcfg_cb)
-    
-    def dyn_rcfg_cb(self, config, level):
-        self.thresh_value = config.thresh
-        self.speed = config.speed
-        self.enable_drive = config.enable_drive
-        return config
+        
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path('traffic_light_pkg')
+        state1_path = f"{package_path}/images/demo_go.jpg"
+        state2_path = f"{package_path}/images/demo_stop.jpg"
+        state1 = cv2.imread(state1_path)
+        state2 = cv2.imread(state2_path)
+        bridge = CvBridge()
+        try:
+            self.go_msg = bridge.cv2_to_imgmsg(state1, encoding="bgr8")
+            self.stop_msg = bridge.cv2_to_imgmsg(state2, encoding="bgr8")
+
+        except CvBridgeError as e:
+            rospy.logerr(f"Error converting images: {e}")
+            return
+        
     
     def publish_time(self):
         rate = rospy.Rate(20)
@@ -67,15 +84,21 @@ class RSUManager:
             
             self.north_pub.publish(1)
             self.south_pub.publish(1)
+            self.north_image.publish(self.go_msg)
+            self.south_image.publish(self.go_msg)
             self.timeN = rospy.Time.from_sec(rospy.Time.now().to_sec() + self.green_dur)
             self.timeS = rospy.Time.from_sec(rospy.Time.now().to_sec() + self.green_dur)
             rospy.sleep(self.green_dur)
             
             self.north_pub.publish(0)
             self.south_pub.publish(0)
+            self.north_image.publish(self.stop_msg)
+            self.south_image.publish(self.stop_msg)
             self.timeN = rospy.Time.from_sec(rospy.Time.now().to_sec() + self.all_red_dur * 2 + self.green_dur)
             self.timeS = rospy.Time.from_sec(rospy.Time.now().to_sec() + self.all_red_dur * 2 + self.green_dur)
             rospy.sleep(self.all_red_dur * 2 + self.green_dur)
+            
+        
 
 
 if __name__ == '__main__':
