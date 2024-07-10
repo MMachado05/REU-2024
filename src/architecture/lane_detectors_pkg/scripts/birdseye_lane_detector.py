5c78a8d76c0f590ddf85f49950ba81c33417f31e
TatianaRas <tanya.rastoskueva@gmail.com>
2024-07-10 11:39:46 -0400

    Adding birdseye to architecture

--- src/architecture/lane_detectors_pkg/scripts/birdseye_lane_detector.py
+++ src/architecture/lane_detectors_pkg/scripts/birdseye_lane_detector.py
@@ -4,6 +4,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 import cv2 as cv
+import numpy as np
+import math
 
 # TODO: Add any imports that the implementation will need
 
@@ -13,9 +15,9 @@ from lane_detectors_pkg.cfg import BirdseyeLaneDetectorConfig  # packageName.cfg
 
 class BirdseyeLaneDetector:
     """
-    K-means lane detector node.
+    Birds Eye lane detector node.
 
-    Uses Beñat's "kmeans_lane_follow.py" code.
+    Uses "bird_eye_houghs.py" code.
     """
 
     preprocessed_img_subscriber: rospy.Subscriber
@@ -39,7 +41,7 @@ class BirdseyeLaneDetector:
         Initializes the node.
         """
         # Node architecture
-        rospy.init_node("kmeans_lane_detector", anonymous=True)
+        rospy.init_node("birds_eye", anonymous=True)
 
         self.preprocessed_img_subscriber = rospy.Subscriber(
             rospy.get_param("~img_in_topic"), Image, self._find_lane
@@ -55,6 +57,10 @@ class BirdseyeLaneDetector:
         self.display_desired_twist_image = False
 
         self.minimum_lane_gap = 100
+        
+        #dynamic reconfiguration params
+        self.thresh = 200
+        self.pers = 0.17
 
         # Misc.
         self.rosimg_cv_bridge = CvBridge()
@@ -76,10 +82,33 @@ class BirdseyeLaneDetector:
             The new dynamic reconfigure parameters.
         """
         self.display_desired_twist_image = config.display_desired_twist_image
+        
 
         # FIXME: Need to implement all of this
+        
+        self.thresh = config.thresh
+        self.pers = config.pers
+        self.crop_hor = config.crop_lines_hor
+        self.crop_ver = config.crop_lines_ver
+        self.crop_orig = config.crop_orig
+        
 
         return config
+
+    def change_perspective(self, img):
+        rows, cols, _ = img.shape
+        
+        p1 = [0, 0]
+        p2 = [cols//2, 0]
+        p3 = [cols//2 - int(cols * self.pers), rows]
+        p4 = [int(cols * self.pers), rows]
+        
+        per1 = np.float32([[0, 0], [cols, 0], [cols, rows], [0, rows]])
+        per2 = np.float32([p1, p2, p3, p4])
+        
+        matrix = cv.getPerspectiveTransform(per1, per2)
+        result = cv.warpPerspective(img, matrix, (cols//2, rows))
+        return result
 
     # ---------------------------------------
     # ----------- Lane detection ------------
@@ -102,22 +131,193 @@ class BirdseyeLaneDetector:
             The preprocessed image to detect the lane in.
         """
         try:
-            grayscale_cv_image = self.rosimg_cv_bridge.imgmsg_to_cv2(
+            cv_image = self.rosimg_cv_bridge.imgmsg_to_cv2(
                 ros_image
-            )  # passthrough
-            grayscale_cv_image = grayscale_cv_image.copy()
-            color_cv_image = cv.cvtColor(grayscale_cv_image, cv.COLOR_GRAY2BGR)
+            )
+            # passthrough
+            # grayscale_cv_image = grayscale_cv_image.copy()
+            if len(cv_image.shape) < 3:
+                cv_image = cv.cvtColor(cv_image, cv.COLOR_GRAY2BGR)
+            self.process_image(cv_image)
         except CvBridgeError as e:
-            rospy.logerr(f"kmeans_lane_detector:116 - CvBridge Error: {e}")
+            rospy.logerr(f"birdseye_lane_detector:116 - CvBridge Error: {e}")
             self.offset_message.angular.z = 3
             self.center_offset_publisher.publish(self.offset_message)
             return
 
-        rows, cols = grayscale_cv_image.shape
+        rows, cols, ch = cv_image.shape
         image_midpoint_x = cols // 2
         image_midpoint_y = rows // 2
 
         # FIX: Literally need to implement all of this
+    def process_image(self, cv_image):
+        # get image shape
+        (rows, cols, _) = cv_image.shape
+        
+        # crop image
+        crop_bound = int(self.crop_orig * rows)
+        cv_image = cv_image[crop_bound : rows, : cols] 
+        cv.imshow("Original", cv_image)    
+        
+        #change perspective
+        cv_image = self.change_perspective(cv_image)
+        cv.imshow("Bird Eye", cv_image)
+        
+        # blur
+        blur = cv.medianBlur(cv_image, 5)
+        
+        # gray image
+        # gray_image = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
+        gray_image = blur.copy()
+        
+        #thresh
+        ret, thresh_basic = cv.threshold(gray_image, # input image
+                                    self.thresh,
+                                    255,    # max value in image
+                                    cv.THRESH_BINARY) 
+        #blur        
+        thresh_basic = cv.medianBlur(thresh_basic, 5)
+        
+        #canny
+        img_canny = cv.Canny(thresh_basic, 50, 150, 3)
+        
+        img_canny = self.houghs(img_canny)
+        
+        cv.imshow("Canny", img_canny)
+        
+        # compute coefficient and publish velocity
+        self.compute_center(img_canny)
+    
+    def houghs(self, image):
+    
+        lines = cv.HoughLinesP(image, 3, np.pi / 180, threshold=20, minLineLength=10, maxLineGap=10)
+        
+        # empty image to draw filtered lines
+        line_image = np.zeros_like(image)
+
+        # filter out lines with a small slope
+        if lines is not None:
+            for line in lines:
+
+                x1, y1, x2, y2 = line[0]
+                
+                # Calculate length of the line segment
+                length = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+                
+                # Calculate slope of the line
+                if x2 - x1 == 0:
+                    slope = float('inf')
+                else:
+                    slope = (y2 - y1) / (x2 - x1)
+                
+                # Extend the line if its length is shorter than 40 pixels and slope is >= 0.4 or <= -0.4
+                if length < 100 and abs(slope) >= 1.5: # 50, 0.5
+                    # Extend the line by a factor of 1.5 times its original length
+                    extend_factor = 2
+                    x1_extended = int(x1 - (x2 - x1) * (extend_factor - 1))
+                    y1_extended = int(y1 - (y2 - y1) * (extend_factor - 1))
+                    x2_extended = int(x2 + (x2 - x1) * (extend_factor - 1))
+                    y2_extended = int(y2 + (y2 - y1) * (extend_factor - 1))
+                    
+                    cv.line(line_image, (x1_extended, y1_extended), (x2_extended, y2_extended), (255, 255, 255), 2)
+                elif abs(slope) >= 1.5:
+                    cv.line(line_image, (x1, y1), (x2, y2), (255, 255, 255), 2)
+        return line_image
+    
+    def compute_y(self, m, c ,x):
+        return int(m * x + c)
+
+    def compute_center(self, image):
+        # crop image
+        rows, cols = image.shape
+        left_bound = cols//2 - int(cols * self.crop_hor / 2)
+        right_bound = cols//2 + int(cols * self.crop_hor / 2)
+        lower_crop = int(rows * self.crop_ver)
+        image = image[lower_crop:, left_bound:right_bound]
+        r, c = image.shape
+        
+        # get all white points (edge lane points)
+        lane_points = cv.findNonZero(image)
+        
+        # get coordinates of all left and right lane points
+        left_x = []
+        left_y = []
+        right_x = []
+        right_y = []
+        if lane_points is not None:
+            for point in lane_points:
+                pnt = point[0]
+                if pnt[0] < c//2:
+                    left_y.append(pnt[0])
+                    left_x.append(pnt[1])
+                else:
+                    right_y.append(pnt[0])
+                    right_x.append(pnt[1])
+        else:
+            self.publish_angle(None, 2)
+            return 
+        
+        # get coefficients for lsrl for left lane
+        try:
+            A = np.vstack([left_x, np.ones(len(left_x))]).T
+            m_left, c_left = np.linalg.lstsq(A, left_y)[0]
+        except:
+            m_left = 0
+            c_left = 0
+        # draw left line
+        cv.line(image, (self.compute_y(m_left, c_left, 0), 0), (self.compute_y(m_left, c_left, r), r), 120, 3)
+        
+        # get coefficients for lsrl for right lane
+        try:
+            A = np.vstack([right_x, np.ones(len(right_x))]).T
+            m_right, c_right = np.linalg.lstsq(A, right_y)[0]
+        except:
+            m_right = 0
+            c_right = c
+        #draw right line
+        cv.line(image, (self.compute_y(m_right, c_right, 0), 0), (self.compute_y(m_right, c_right, r), r), 170, 3)
+        
+        # compute midpoint of two lanes
+        y_left = self.compute_y(m_left, c_left, r//2)
+        if y_left > c//2:
+            y_left = 0
+        
+        y_right = self.compute_y(m_right, c_right, r//2)
+        if y_right < c//2:
+            y_right = c
+        
+        cx = int((y_left+y_right)//2)
+        cy = r // 2
+        mid = c // 2
+        
+        angle = float(math.degrees(math.atan2(abs(mid - cx), abs(rows - cy))) / 2) * 10 / 180
+        
+        if cx < mid:
+            self.publish_angle(-angle, 0)
+        else:
+            self.publish_angle(angle, 0)
+
+        # draw, show & return result
+        cv.circle(image, (cx, cy), 3, 255, -1)    
+        cv.imshow("Lines", image)
+        cv.waitKey(3)
+        
+    def publish_angle(self, coeff, missed_lanes):
+        
+        if missed_lanes == 2:
+            self.offset_message.angular.z = 2
+        elif missed_lanes == 1:
+            self.offset_message.angular.z = 1
+        else:
+            if (coeff > 1):
+                self.offset_message.angular.x = 1
+            elif (coeff < -1):
+                self.offset_message.angular.x = -1
+            else:
+                self.offset_message.angular.x = coeff
+            self.offset_message.angular.z = 0
+        
+        self.center_offset_publisher.publish(self.offset_message)
 
 
 if __name__ == "__main__":
